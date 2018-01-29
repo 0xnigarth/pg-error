1e09d86e036f710864c7063ec7ca2b6de670002b
Cesar Nava <Oxnigarth@Cesars-MacBook-Air.local>
2018-01-29 13:14:43 -0600

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home', as: "root"
   get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
+  
 end
