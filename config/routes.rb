31c149181c222858203be91fed2787e9f685e143
Cesar Nava <Oxnigarth@Cesars-MacBook-Air.local>
2018-01-30 12:05:08 -0600

    Add Password Reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+ 
+
   get 'sessions/new'
 
   get 'users/new'
@@ -19,4 +21,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
